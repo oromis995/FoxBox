f683d6d80723f586d0ece51988508766c7046fb7
oromis995 <54419741+oromis995@users.noreply.github.com>
2021-12-04 02:52:24 -0600

    Base App
    Decided on background color and made logo

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity,fontFamily, View, Image, SafeAreaView, Platform } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log("Text Pressed")
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity onPress={() => console.log("Image Tapped")}>
+        <Image //style={styles.logo}
+          source={require('./assets/FoxBoxLogo.png')}
+          blurRadius={0.5}
+        />
+      </TouchableOpacity>
+      <Text style={styles.title}>FoxBox </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#701d0f',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    fontStyle:'normal',
+    color: 'white',
+    fontSize: 36,
+    fontWeight:'bold',
+    fontFamily: Platform.OS === 'ios' ? 'San Francisco' : 'Roboto'
+  },
+  logo:{
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  }
 });
